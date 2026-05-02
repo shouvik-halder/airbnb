5bfca55fe0bda03c3414f06b14e376d13029b110
shouvik-halder <shouvik2707@gmail.com>
2026-05-02 11:14:37 +0530

    Moved the utils methods into Utils Folder

--- AuthenticationService/services/user.service.go
+++ AuthenticationService/services/user.service.go
@@ -3,13 +3,8 @@ package services
 import (
 	db "AuthenticationService/db/repositories"
 	"AuthenticationService/model"
-	"crypto/pbkdf2"
-	"crypto/rand"
-	"crypto/sha256"
-	"crypto/subtle"
-	"encoding/base64"
+	"AuthenticationService/utils"
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -56,7 +51,7 @@ func (userService *userServiceImpl) Register(email, password string) (*AuthRespo
 		return nil, err
 	}
 
-	hashedPassword, err := hashPassword(password)
+	hashedPassword, err := utils.HashPassword(password)
 	if err != nil {
 		return nil, err
 	}
@@ -83,7 +78,7 @@ func (userService *userServiceImpl) Login(email, password string) (*AuthResponse
 		return nil, err
 	}
 
-	ok, err := verifyPassword(password, user.PasswordHash)
+	ok, err := utils.VerifyPassword(password, user.PasswordHash)
 	if err != nil || !ok {
 		return nil, ErrInvalidCredentials
 	}
@@ -152,53 +147,7 @@ func (userService *userServiceImpl) generateToken(user *model.User) (string, err
 	return token.SignedString([]byte(userService.tokenSecret))
 }
 
-func hashPassword(password string) (string, error) {
 
-	salt := make([]byte, 16)
-	if _, err := rand.Read(salt); err != nil {
-		return "", err
-	}
-
-	key, err := pbkdf2.Key(sha256.New, password, salt, 210000, 32)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf(
-		"pbkdf2_sha256$210000$%s$%s",
-		base64.RawStdEncoding.EncodeToString(salt),
-		base64.RawStdEncoding.EncodeToString(key),
-	), nil
-}
-
-func verifyPassword(password, storedHash string) (bool, error) {
-	parts := strings.Split(storedHash, "$")
-	if len(parts) != 4 || parts[0] != "pbkdf2_sha256" {
-		return false, nil
-	}
-
-	var iterations int
-	if _, err := fmt.Sscanf(parts[1], "%d", &iterations); err != nil {
-		return false, err
-	}
-
-	salt, err := base64.RawStdEncoding.DecodeString(parts[2])
-	if err != nil {
-		return false, err
-	}
-
-	expectedKey, err := base64.RawStdEncoding.DecodeString(parts[3])
-	if err != nil {
-		return false, err
-	}
-
-	actualKey, err := pbkdf2.Key(sha256.New, password, salt, iterations, len(expectedKey))
-	if err != nil {
-		return false, err
-	}
-
-	return subtle.ConstantTimeCompare(actualKey, expectedKey) == 1, nil
-}
 
 func NewUserService(_userRepository db.UserRepository, tokenSecret string) UserService {
 	return &userServiceImpl{
---
+++ AuthenticationService/utils/auth.utils.go
@@ -0,0 +1,63 @@
+package utils
+
+import (
+	"crypto/pbkdf2"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+)
+
+
+func HashPassword(password string) (string, error) {
+
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	key, err := pbkdf2.Key(sha256.New, password, salt, 210000, 32)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(
+		"pbkdf2_sha256$210000$%s$%s",
+		base64.RawStdEncoding.EncodeToString(salt),
+		base64.RawStdEncoding.EncodeToString(key),
+	), nil
+}
+
+func VerifyPassword(password, storedHash string) (bool, error) {
+	parts := strings.Split(storedHash, "$")
+	if len(parts) != 4 || parts[0] != "pbkdf2_sha256" {
+		return false, nil
+	}
+
+	var iterations int
+	if _, err := fmt.Sscanf(parts[1], "%d", &iterations); err != nil {
+		return false, err
+	}
+
+	salt, err := base64.RawStdEncoding.DecodeString(parts[2])
+	if err != nil {
+		return false, err
+	}
+
+	expectedKey, err := base64.RawStdEncoding.DecodeString(parts[3])
+	if err != nil {
+		return false, err
+	}
+
+	actualKey, err := pbkdf2.Key(sha256.New, password, salt, iterations, len(expectedKey))
+	if err != nil {
+		return false, err
+	}
+
+	return subtle.ConstantTimeCompare(actualKey, expectedKey) == 1, nil
+}
+
+
